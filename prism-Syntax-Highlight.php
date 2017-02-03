21a94b8f5751e1c8979ef8342eb103d6cb772554
Poul Serek <pps@netcompany.com>
2017-02-03 19:23:11 +0100

    Update prism-Syntax-Highlight.php

--- prism-Syntax-Highlight.php
+++ prism-Syntax-Highlight.php
@@ -1,7 +1,7 @@
 <?php
    /*
    Plugin Name: Load Prism Syntax Highlight On-Demand
-   Plugin URI: https://odd-one-out.serek.eu
+   Plugin URI: https://odd-one-out.serek.eu/prism-wordpress-without-plugin/
    Description: A simple plugin to load Prism Syntax Highlight on-demand. The plugin looks for "<code class="language-" tags in the post and loads the needed JS / CSS only when needed. The JS and CSS is generated from http://prismjs.com. Replace the prism.css and prism.js with you own.
    Version: 1.0
    Author: Poul Serek
